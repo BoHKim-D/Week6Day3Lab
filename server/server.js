832d2926332287f5fdddfd5f17de4c9fbc0e84a2
Bo Kim <bohyung0110@gmail.com>
2022-03-16 13:12:35 -0700

    v0.04

--- server/server.js
+++ server/server.js
@@ -1,11 +1,11 @@
-const express = require('express')
-const cors = require('cors')
-const path = require('path')
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
 app.use(express.static(path.join(__dirname, "../public")));
 
@@ -13,4 +13,4 @@ const port = process.env.PORT || 4005;
 
 app.listen(port, () => {
     console.log(`We are online on ${port}`)
-})
+});
