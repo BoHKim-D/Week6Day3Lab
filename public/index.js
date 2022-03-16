7ea33ea2e1a6df7e0b17d6003730cf938a696baf
Bo Kim <bohyung0110@gmail.com>
2022-03-16 14:42:54 -0700

    v0.08

--- public/index.js
+++ public/index.js
@@ -1,8 +1,20 @@
 let button = document.getElementById('deletes')
 let video = document.getElementById('myVid')
 
+
 function deletes() {
-    video.remove();
-};
+    if(button.id === 'deletes') {
+        video.removeAttribute("src");
+        button.innerText = "ADD IT BACK";
+        button.setAttribute("id", "addb")
+    } else if(button.id === "addb"){
+        button.innerText = "Delete this stupid video."
+        button.setAttribute("id", "deletes")
+        video.setAttribute("src","https://www.youtube.com/embed/U9e3MFZI3zE")
+    }
+}
 
 button.addEventListener('click', deletes)
+
+
+
