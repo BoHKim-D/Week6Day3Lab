e5111bfa5e5fdfe4f096702a7b0c0acb5a696b62
Bo Kim <bohyung0110@gmail.com>
2022-03-16 13:17:11 -0700

    v0.05

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use(express.static(path.join(__dirname, "../public")));
+app.use("/images", express.static(path.join(__dirname, "../images")));
 
 const port = process.env.PORT || 4005;
 
